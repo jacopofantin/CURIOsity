c8ebc5df1acdedc0c707fd5fca421404a93fa246
yacoo <yacoo@JACU-2020>
2025-08-13 17:10:07 +0200

    Projektdateien hinzufügen.

---
+++ CuriosityForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CURIOsity
+{
+    partial class CuriosityForm
+    {
+        // variable declaration
+
+        // ListBox variables for viewing stagecraft equipment, left panel array and right panel array positions
+        private System.Windows.Forms.ListBox stagecraftListBox;
+        private System.Windows.Forms.ListBox leftPanelsListBox;
+        private System.Windows.Forms.ListBox rightPanelsListBox;
+
+        // ListBox titles
+        private System.Windows.Forms.Label stagecraftListBoxLabel;
+        private System.Windows.Forms.Label leftPanelsListBoxLabel;
+        private System.Windows.Forms.Label rightPanelsListBoxLabel;
+
+        // "Load text file" button variable
+        private System.Windows.Forms.Button loadTextButton;
+
+        // "Load BIM file" button variable
+        private System.Windows.Forms.Button loadBimButton;
+
+        private void InitializeComponent()
+        {
+            // element creation
+
+            // ListBox creation
+            this.stagecraftListBox = new System.Windows.Forms.ListBox();
+            this.leftPanelsListBox = new System.Windows.Forms.ListBox();
+            this.rightPanelsListBox = new System.Windows.Forms.ListBox();
+
+            // ListBox titles creation
+            this.stagecraftListBoxLabel = new System.Windows.Forms.Label();
+            this.leftPanelsListBoxLabel = new System.Windows.Forms.Label();
+            this.rightPanelsListBoxLabel = new System.Windows.Forms.Label();
+
+            // "Load text file" button creation
+            this.loadTextButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // "Load BIM file" button creation
+            this.loadBimButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+
+            // element initialization
+
+            // ListBox for stagecraft equipment initialization
+            this.stagecraftListBox.Location = new System.Drawing.Point(12, 80);
+            this.stagecraftListBox.Size = new System.Drawing.Size(240, 200);
+            this.stagecraftListBox.Name = "stagecraftListBox";
+
+            // ListBox for left panel arrays initialization
+            this.leftPanelsListBox.Location = new System.Drawing.Point(270, 80);
+            this.leftPanelsListBox.Size = new System.Drawing.Size(240, 200);
+            this.leftPanelsListBox.Name = "leftPanelListBox";
+
+            // ListBox for right panel arrays initialization
+            this.rightPanelsListBox.Location = new System.Drawing.Point(528, 80);
+            this.rightPanelsListBox.Size = new System.Drawing.Size(240, 200);
+            this.rightPanelsListBox.Name = "rightPanelListBox";
+
+            // title for stagecraft equipment ListBox initialization
+            this.stagecraftListBoxLabel.Location = new System.Drawing.Point(12, 60);
+            this.stagecraftListBoxLabel.Size = new System.Drawing.Size(240, 20);
+            this.stagecraftListBoxLabel.Text = "Stagecraft equipment positions";
+            this.stagecraftListBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+
+            // title for left panel array ListBox initialization
+            this.leftPanelsListBoxLabel.Location = new System.Drawing.Point(270, 60);
+            this.leftPanelsListBoxLabel.Size = new System.Drawing.Size(240, 20);
+            this.leftPanelsListBoxLabel.Text = "Left panel array positions";
+            this.leftPanelsListBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+
+            // title for right panel array ListBox initialization
+            this.rightPanelsListBoxLabel.Location = new System.Drawing.Point(528, 60);
+            this.rightPanelsListBoxLabel.Size = new System.Drawing.Size(240, 20);
+            this.rightPanelsListBoxLabel.Text = "Right panel array positions";
+            this.rightPanelsListBoxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+
+            // "Load text file" button initialization
+            this.loadTextButton.Location = new System.Drawing.Point(12, 300); // below the ListBoxes
+            this.loadTextButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.loadTextButton.Name = "loadTextButton";
+            this.loadTextButton.Size = new System.Drawing.Size(120, 30);
+            this.loadTextButton.TabIndex = 1;
+            this.loadTextButton.Text = "Load text file";
+            this.loadTextButton.UseVisualStyleBackColor = true;
+            this.loadTextButton.Click += new System.EventHandler(this.LoadTextButton_OnClick);
+
+            // "Load BIM file" button initialization
+            this.loadBimButton.Location = new System.Drawing.Point(150, 300); // below the ListBoxes
+            this.loadBimButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.loadBimButton.Name = "loadBimButton";
+            this.loadBimButton.Size = new System.Drawing.Size(120, 30);
+            this.loadBimButton.TabIndex = 2;
+            this.loadBimButton.Text = "Load BIM file";
+            this.loadBimButton.UseVisualStyleBackColor = true;
+            this.loadBimButton.Click += new System.EventHandler(this.LoadBimButton_OnClick);
+
+            // Form initialization
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.stagecraftListBox);
+            this.Controls.Add(this.leftPanelsListBox);
+            this.Controls.Add(this.rightPanelsListBox);
+            this.Controls.Add(this.stagecraftListBoxLabel);
+            this.Controls.Add(this.leftPanelsListBoxLabel);
+            this.Controls.Add(this.rightPanelsListBoxLabel);
+            this.Controls.Add(this.loadTextButton);
+            this.Controls.Add(this.loadBimButton);
+            this.Name = "CURIOsity";
+            this.Text = "CURIOsity";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
+
---
+++ CuriosityForm.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Xbim.Ifc;
+using Xbim.Ifc4.Interfaces;
+using static System.Windows.Forms.LinkLabel;
+
+namespace CURIOsity
+{
+    public partial class CuriosityForm : Form
+    {
+        // class-level dictionaries to store moving element positions
+        Dictionary<string, int> stagecraftEquipmentPositions = new Dictionary<string, int>();
+        Dictionary<string, int> leftPanelPositions = new Dictionary<string, int>();
+        Dictionary<string, int> rightPanelPositions = new Dictionary<string, int>();
+        public CuriosityForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadTextButton_OnClick(object sender, EventArgs e)
+        {
+            using (OpenFileDialog loadTextDialog = new OpenFileDialog())
+            {
+                loadTextDialog.Filter = "Text file (*.txt)|*.txt|All files (*.*)|*.*";
+                if (loadTextDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName = loadTextDialog.FileName;
+                    try
+                    {
+                        foreach (var line in File.ReadLines(fileName).Skip(3).Take(13))
+                        {
+                            stagecraftEquipmentPositions.Add(line.Substring(0, 6), int.Parse(line.Substring(9, 5)));
+                        }
+
+                        foreach(var line in File.ReadLines(fileName).Skip(17).Take(26))
+                        {
+                            leftPanelPositions.Add(line.Substring(0, 6), int.Parse(line.Substring(9, 5)));
+                        }
+
+                        foreach (var line in File.ReadLines(fileName).Skip(44).Take(26))
+                        {
+                            rightPanelPositions.Add(line.Substring(0, 6), int.Parse(line.Substring(9, 5)));
+                        }
+
+                        // empty and refill ListBoxes
+                        stagecraftListBox.Items.Clear();
+                        foreach (var pair in stagecraftEquipmentPositions)
+                            stagecraftListBox.Items.Add($"{pair.Key}: {pair.Value} mm");
+
+                        leftPanelsListBox.Items.Clear();
+                        foreach (var pair in leftPanelPositions)
+                            leftPanelsListBox.Items.Add($"{pair.Key}: {pair.Value} mm");
+
+                        rightPanelsListBox.Items.Clear();
+                        foreach (var pair in rightPanelPositions)
+                            rightPanelsListBox.Items.Add($"{pair.Key}: {pair.Value} mm");
+                    }
+                    catch (Exception error)
+                    {
+                        MessageBox.Show("Error while reading text file: " + error.Message + "Please make sure the text file is formatted as expected (described in the readme file)");
+                    }
+                }
+            }
+        }
+
+        private void LoadBimButton_OnClick(object sender, EventArgs e)
+        {
+            using (OpenFileDialog loadBimDialog = new OpenFileDialog())
+            {
+                loadBimDialog.Filter = "BIM file (*.bim)|*.bim|All files (*.*)|*.*";
+                if (loadBimDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName = loadBimDialog.FileName;
+                    try
+                    {
+                        using (var model = IfcStore.Open(fileName))
+                        {
+                            foreach (var pair in stagecraftEquipmentPositions)
+                            {
+                                //model.Instances.OfType<IIfcProduct>() = pair.Value; // writes the position of each stagecraft equipment piece in the BIM model
+                            }
+                        }
+                    }
+                    catch (Exception error)
+                    {
+                        MessageBox.Show("Error while reading BIM file: " + error.Message);
+                    }
+                }
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            // ListBoxes size and distance between them
+            int listBoxWidth = stagecraftListBox.Width;
+            int listBoxHeight = stagecraftListBox.Height;
+            int listBoxSpacing = 18; // distance between the ListBoxes
+            int totalWidth = listBoxWidth * 3 + listBoxSpacing * 2;
+            int totalHeight = listBoxHeight; // same height for all ListBoxes
+
+            // Starting points for horizontal and vertical centering
+            int startX = (this.ClientSize.Width - totalWidth) / 2;
+            int startY = (this.ClientSize.Height - totalHeight) / 2;
+
+            // ListBoxes positions
+            stagecraftListBox.Left = startX;
+            stagecraftListBox.Top = startY;
+
+            leftPanelsListBox.Left = startX + listBoxWidth + listBoxSpacing;
+            leftPanelsListBox.Top = startY;
+
+            rightPanelsListBox.Left = startX + (listBoxWidth + listBoxSpacing) * 2;
+            rightPanelsListBox.Top = startY;
+
+            // move TextBox titles accordingly, when existing
+            if (stagecraftListBoxLabel != null)
+            {
+                stagecraftListBoxLabel.Left = stagecraftListBox.Left;
+                stagecraftListBoxLabel.Top = stagecraftListBox.Top - stagecraftListBoxLabel.Height - 4;
+            }
+            if (leftPanelsListBoxLabel != null)
+            {
+                leftPanelsListBoxLabel.Left = leftPanelsListBox.Left;
+                leftPanelsListBoxLabel.Top = leftPanelsListBox.Top - leftPanelsListBoxLabel.Height - 4;
+            }
+            if (rightPanelsListBoxLabel != null)
+            {
+                rightPanelsListBoxLabel.Left = rightPanelsListBox.Left;
+                rightPanelsListBoxLabel.Top = rightPanelsListBox.Top - rightPanelsListBoxLabel.Height - 4;
+            }
+
+            // horizontally centering "Load text file" and "Load BIM file" buttons and keeping them one beside the other at the bottom of the form
+            int buttonSpacing = 16; // distance between buttons
+            int buttonsTotalWidth = loadTextButton.Width + buttonSpacing + loadBimButton.Width;
+            int buttonsStartX = (this.ClientSize.Width - buttonsTotalWidth) / 2;
+            int buttonsY = this.ClientSize.Height - loadTextButton.Height - 24; // 24 pixels from the bottom of the form
+
+            loadTextButton.Left = buttonsStartX;
+            loadTextButton.Top = buttonsY;
+            
+            loadBimButton.Left = buttonsStartX + loadTextButton.Width + buttonSpacing;
+            loadBimButton.Top = buttonsY;
+        }
+    }
+}
